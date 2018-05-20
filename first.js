c44e867dfd06de87561e3216c961b14cb7ad6887
ZhangZisu <zhangzisu@outlook.com>
2018-05-20 08:08:28 +0800

    Update

--- first.js
+++ first.js
@@ -2,7 +2,7 @@ let fs = require('fs-extra');
 let conif = require('node-console-input');
 
 module.exports = function () {
-	let config = JSON.parse(fs.readFileSync('config.example.json')), result;
+	let config = JSON.parse(fs.readFileSync('config.example.json')), result, enableOptional;
 	config.title = conif.getConsoleInput('Site title: ').trim();
 	config.listen = conif.getConsoleInput('Listen address: ').trim();
 	config.port = conif.getConsoleInput('Listen port: ').trim();
@@ -22,13 +22,20 @@ module.exports = function () {
 		config.db.username = conif.getConsoleInput('Mysql database username: ').trim();
 		config.db.password = conif.getConsoleInput('Mysql database password: ').trim();
 		config.db.host = conif.getConsoleInput('Mysql server address: ').trim();
+		enableOptional = false;
 	} else if (result === 'sqlite') {
 		config.db.dialect = 'sqlite';
 		config.db.storage = conif.getConsoleInput('Database file place: ').trim();
+		enableOptional = true;
 	}
 
 	result = conif.getConsoleInput('ZOJ Token: ').trim();
 	config.token = config.secret = result;
 	fs.writeFileSync('config.json', JSON.stringify(config, null, '\t'));
+	let upgrade = 'git fetch --all && git reset --hard origin/master\n';
+	if (enableOptional) upgrade += 'npm install --production\n';
+	else upgrade += 'npm install --production --no-optional\n';
+	upgrade += 'killall node\nnode app.js >log.txt';
+	fs.writeFileSync('upgrade.sh', upgrade);
 	return config;
 };
